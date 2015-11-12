9f17a1ac605053b28e50297917102c8f3642e521
Sushant Chavan <sushant@prototyze.com>
2015-11-12 11:32:56 +0530

    add end points

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+	res.send('service working');
+})
 router.post('/invite', function(req, res, next) {
   var data = req.body
   console.log('This is the request coming from mailchimp')
   console.log(data);
+  res.send('POST request works');
+});
+
+router.get('/invite', function(req, res, next) {
+	var data = req.body;
+	res.send('GET request works');
 });
 
 module.exports = router;
