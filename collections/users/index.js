7bf6b8b2f23c1c6593d2220ff507d2a416288d46
Sushant Chavan <sushant@prototyze.com>
2015-11-13 16:42:50 +0530

    replace callback with user

--- collections/users/index.js
+++ collections/users/index.js
@@ -34,7 +34,7 @@ function sendSlackinvite (user, callback) {
 
 var self = module.exports = {
     register: function(data, callback) {
-    	User.findOne({'email': data['email']}, function(err, callback){
+    	User.findOne({'email': data['email']}, function(err, user){
     		if(err){console.log(err); callback(err, null);}
     		else if(user) {
     			if(user.slackinvite) {
